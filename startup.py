a12759b5802695da93c63c0e7e68e945ccfee867
Markus Paasche <markus.paasche@gmail.com>
2022-12-19 15:43:17 +0100

    creating api

--- startup.py
+++ startup.py
@@ -1,10 +1,12 @@
 import Dashboard
 import FastAPI
 import multiprocessing
+import api.api as api
 
 
 jobs = []
 jobs.append(multiprocessing.Process(target=FastAPI.start))
+jobs.append(multiprocessing.Process(target=api.start))
 jobs.append(multiprocessing.Process(target=Dashboard.start))
 
 
