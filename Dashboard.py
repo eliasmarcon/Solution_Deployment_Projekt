4d8db3ca54976917a40b62f89348b6d838f92e1b
Markus Paasche <markus.paasche@gmail.com>
2022-12-20 17:55:38 +0100

    integrative for docker

--- Dashboard.py
+++ Dashboard.py
@@ -64,13 +64,13 @@ app.layout = dbc.Container(
 
 # Run the app
 def start():    
-    # app.run_server(host = '0.0.0.0', port = 80)
+    app.run_server(host = '0.0.0.0', port = 80)
     return 'Done!'
 
 if __name__ == '__main__':
-    # start()
+    start()
     #app.run_server(host = '0.0.0.0', port = 80)
-    app.run_server(debug = True, port = "8051")
+    #app.run_server(debug = True, port = "8051")
 
 
 
--- startup.py
+++ startup.py
@@ -1,11 +1,9 @@
 import Dashboard
-import FastAPI
 import multiprocessing
 import api
 
 
 jobs = []
-jobs.append(multiprocessing.Process(target=FastAPI.start))
 jobs.append(multiprocessing.Process(target=api.start))
 jobs.append(multiprocessing.Process(target=Dashboard.start))
 
